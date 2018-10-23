2936c7e44fc7b8993d2e6c2e0be1f5825fa6d381
Acep Abdurohman <acepabdurohman90@gmail.com>
2018-10-23 11:27:26 +0700

    change to default object 3.5

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,6 @@
 from flask import Flask, request, jsonify
-#from flask_sqlalchemy import SQLAlchemy
-#from flask_marshmallow import Marshmallow
-from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///dbidentity'
